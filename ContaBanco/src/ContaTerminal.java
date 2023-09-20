767c0fa2965aace0433918701e7d7e8ea6858505
Paulo Henrique <sousa.pauloh@gmail.com>
2023-09-20 10:25:10 -0300

    pronto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o número da sua conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite o número da sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o valor do deposito inicial: ");
+        float dep = scanner.nextFloat();
+
+        System.out.println("Olá " + nome +
+                ", obrigado por criar uma conta em nosso banco," +
+                " sua agência é " + agencia + ", conta " + conta + " e seu saldo "
+                + dep + " já está disponível para saque. ");
+    }
 }
